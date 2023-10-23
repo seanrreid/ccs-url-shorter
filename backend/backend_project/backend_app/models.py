2b56eb2db9e1c36f06467261e45e8ca09df46d86
Sean R Reid <sean@torchcodelab.com>
2023-10-23 15:52:29 -0400

    Created Urls model, added necessary changes to admin route, ran migrations

--- backend/backend_project/backend_app/models.py
+++ backend/backend_project/backend_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+
+class Url(models.Model):
+    title = models.CharField()
+    original_url = models.CharField()
+    short_url = models.CharField(max_length=256)
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ["title"]
--- backend/backend_project/urls.py
+++ backend/backend_project/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from backend_project.backend_app import views
+
+router = routers.DefaultRouter()
+# router.register(r'ceos', views.CeoViewSet)
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('admin/', admin.site.urls),
+    # path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
