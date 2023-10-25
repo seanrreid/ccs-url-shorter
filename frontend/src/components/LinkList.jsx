2943bdbae641b3905cff9b74a432f3167f66274a
Sean R Reid <sean@torchcodelab.com>
2023-10-25 08:12:07 -0400

    Updated frontend to load env variables for api URL

--- frontend/src/components/LinkList.jsx
+++ frontend/src/components/LinkList.jsx
@@ -1,11 +1,12 @@
 /* eslint-disable react/prop-types */
 
 export default function LinkList({ linkList }) {
+    const src_url = import.meta.env.VITE_SOURCE_URL;
     return (
         <ul>
             {linkList.map((link, id) => {
                 console.log('LINK IS: ', link);
-                return <li key={id}><a href={`http://127.0.0.1:8000/redirect/${link.short_url}` }>{link.title}</a></li>;
+                return <li key={id}><a href={`${src_url}/snd/${link.short_url}` }>{link.title}</a></li>;
             })}
         </ul>
     );
