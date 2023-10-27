025e862a5f0aadfd8d5fd9577e02dae2f72f53ba
Sean R Reid <sean@torchcodelab.com>
2023-10-27 14:24:41 -0400

    Updated settings for Render

--- backend/backend_project/settings.py
+++ backend/backend_project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'ccs-url-short.onrender.com']
 CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1']
 
 # Application definition
